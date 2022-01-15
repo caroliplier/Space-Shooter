0724b44012d45ed6ec0f808373bdfccf25052fb3
caroliplier <caroline.wijaya002@binus.ac.id>
2022-01-15 20:01:57 +0700

    added live and score

--- alien.py
+++ alien.py
@@ -6,5 +6,9 @@ class Alien(pygame.sprite.Sprite):
         self.image = pygame.image.load('assets/' + colour + 'a.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = (x,y))
     
+        if colour == 'red' : self.value = 100
+        elif colour == 'green' : self.value = 200
+        else: self.value = 300
+
     def update(self,direction):
         self.rect.x += direction
--- main.py
+++ main.py
@@ -15,6 +15,14 @@ class Game:
         bg_sprite = Background((0,0))
         self.background = pygame.sprite.GroupSingle(bg_sprite)
         
+        #health and score setup
+        self.lives = 3
+        self.live_img = pygame.image.load('assets/pixel_ship_yellow_up.png').convert_alpha()
+        self.live_xstart = screen_width - (self.live_img.get_size()[0] * 2 + 20)
+
+        self.score = 0
+        self.font = pygame.font.Font('assets/pixeled.ttf',20)
+
         #player setup
         player_sprite = Player((screen_width/2,screen_height),screen_width,screen_height,5)
         self.player = pygame.sprite.GroupSingle(player_sprite)
@@ -25,7 +33,7 @@ class Game:
         self.alien_direction = 1
         self.alien_lasers = pygame.sprite.Group()
 
-    def alien_setup(self,rows,cols,x_distance = 70,y_distance = 60,x_offset = 80, y_offset = 50):
+    def alien_setup(self,rows,cols,x_distance = 70,y_distance = 60,x_offset = 80, y_offset = 60):
         for row_index, row in enumerate(range(rows)):
             for col_index, col in enumerate(range(cols)):
                 x = col_index * x_distance + x_offset
@@ -60,18 +68,41 @@ class Game:
     def collision_checks(self):
         if self.player.sprite.lasers:
             for laser in self.player.sprite.lasers:
-                if pygame.sprite.spritecollide(laser,self.aliens,True):
+                aliens_hit = pygame.sprite.spritecollide(laser,self.aliens,True)
+                if aliens_hit:
+                    for alien in aliens_hit:
+                        self.score += alien.value
                     laser.kill()
+
         if self.alien_lasers:
             for laser in self.alien_lasers:
                 if pygame.sprite.spritecollide(laser,self.player,False):
                     laser.kill()
-                    print('dead')
+                    self.lives -= 1
+                    if self.lives <= 0:
+                        pygame.quit()
+                        sys.exit()
         if self.aliens:
             for alien in self.aliens:
                 if pygame.sprite.spritecollide(alien,self.player,False):
                     pygame.quit()
                     sys.exit()
+
+    def display_lives(self):
+        for live in range(self.lives - 1):
+            x = self.live_xstart + (live * (self.live_img.get_size()[0] + 10))
+            screen.blit(self.live_img,(x,8))
+
+    def display_score(self):
+        score_surf = self.font.render(f'SCORE: {self.score}',False,'white')
+        score_rect = score_surf.get_rect(topleft = (10,-10))
+        screen.blit(score_surf,score_rect)
+    
+    def victory(self):
+        if not self.aliens.sprites():
+            victory_surf = self.font.render('You WON!',False,'gold')
+            victory_rect = victory_surf.get_rect(center = (screen_width/2, screen_height/2))
+            screen.blit(victory_surf,victory_rect)
 
     def run(self):
         self.background.draw(screen)
@@ -79,14 +110,19 @@ class Game:
         self.player.update()
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
-        
-        self.collision_checks()
 
         self.alien_pos_checker()
         self.aliens.update(self.alien_direction)
         self.alien_lasers.update()
         self.alien_lasers.draw(screen)
         self.aliens.draw(screen)
+        
+        self.collision_checks()
+
+        self.display_lives()
+        self.display_score()
+        
+        self.victory()
 
 if __name__ == '__main__':
     pygame.init()
